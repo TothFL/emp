3cfbf1137f53a045119cd7583ec0e9420b4824ea
Tóth Fedor László <toth.fedor@gmail.com>
2023-02-23 17:54:59 +0100

    Létrehozás

---
+++ src/app.js
@@ -0,0 +1,8 @@
+var emplist = [
+    {id: 1, name: "Per Lajos", city: "Szeged", salary: 345},
+    {id: 2, name: "Para Béla", city: "Szeged", salary: 345},
+    {id: 3, name: "Erős Imre", city: "Szolnok", salary: 325},
+    {id: 4, name: "Tér Emese", city: "Szeged", salary: 385},
+    {id: 5, name: "Ken Irén", city: "Miskolc", salary: 375},
+    {id: 6, name: "Csoma ferenc", city: "Szeged", salary: 395}
+];
